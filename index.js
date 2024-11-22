d87f089d627dc7d1ebfa174818d685da1308af8c
supriadiibbr <148852395+supriadiibbr@users.noreply.github.com>
2024-11-22 21:36:09 +0700

    connectDB

--- index.js
+++ index.js
@@ -4,10 +4,46 @@ const app = express();
 
 const PORT = 3030;
 
-app.get('/', function(req, res){
-    res.send('halo supri ganteng')
-})
+const {Client} = require('pg');
 
+const client = new Client({
+    host: 'localhost',
+    port: '5432',
+    user: 'postgres',
+    password: '',
+    database: 'supriadisql'
+});
+
+client.connect()
+    .then(() => console.log('Connected to PostgresSQL'))
+    .catch(err => console.error('Connection error', err.stack));
+
+// client.query('SELECt NOW()', (err, res) => {
+//     console.log(err ? err.stack : res.rows[0]);
+//     client.end();
+// });
+
+const createTableQuery =`
+    CREATE TABLE pengguna (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT null,
+        email VARCHAR(100) UNIQUE NOT null,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+`;
+    
+    
+
+client.query(createTableQuery)
+    .then(res => {
+        console.log('Table created successfully');
+        })
+    .catch(err => {
+        console.error('Error creating table', err.stack);
+        })
+    .finally(() => {
+        client.end();
+        });
 
 app.listen(PORT, (error) =>{
     if(!error)
